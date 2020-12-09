080cd6018bb5e60a65f8f370b5b440223c36fe64
Enea Jahollari <jahollarienea14@gmail.com>
2020-12-09 23:05:53 +0100

    ✔ Error Alert changes and implementations in Members page

--- src/app/shared/components/error-alert/error-alert.component.ts
+++ src/app/shared/components/error-alert/error-alert.component.ts
@@ -4,8 +4,10 @@ import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
   selector: 'error-alert',
   template: `
     <ng-container *ngIf="error">
-      <div class="alert alert-error error cp mb-1">
+      <div class="alert alert-error error cp mb-1"
+           [ngStyle]="{ display: inline ? 'inline' : 'block' }">
         {{ error }}
+        <ng-content></ng-content>
       </div>
     </ng-container>
   `,
@@ -13,6 +15,16 @@ import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 })
 export class ErrorAlertComponent {
 
+  /**
+  * String error message.
+  * @default null
+  * */
   @Input() error: string | null = null;
+
+  /**
+  * If set to true the error alert box will be inlined.
+  * @default false
+  */
+  @Input() inline: boolean = false;
 
 }
