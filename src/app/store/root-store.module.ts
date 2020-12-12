6b3d4ea5ab594483065dde6b14f28ab1a570f5b6
Enea Jahollari <jahollarienea14@gmail.com>
2020-12-12 02:40:48 +0100

    🐛 Correctly removed StoreDevTools from production build

--- src/app/store/root-store.module.ts
+++ src/app/store/root-store.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 import { EffectsModule } from "@ngrx/effects";
 import { StoreModule } from "@ngrx/store";
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
-import { environment as env } from "../../environments/environment";
 
 import { CustomSerializer, metaReducers, ROOT_REDUCERS } from "@store/reducers";
 import { RootEffects } from "@store/effects/root.effects";
 import { AuthStoreModule } from "@auth/store/auth-store.module";
+import { extModules } from "../build-specifics";
 
 @NgModule({
   imports: [
@@ -20,13 +18,11 @@ import { AuthStoreModule } from "@auth/store/auth-store.module";
     AuthStoreModule,
 
     StoreModule.forRoot(ROOT_REDUCERS, { metaReducers }),
-
     EffectsModule.forRoot([ RootEffects ]),
-
     StoreRouterConnectingModule.forRoot({ serializer: CustomSerializer }),
 
-    // Won't include StoreDevtoolsModule in production build
-    env.production ? [] : StoreDevtoolsModule.instrument({ maxAge: 20 })
+    // Instrumentation must be imported after importing StoreModule
+    extModules
   ]
 })
 export class RootStoreModule { }
