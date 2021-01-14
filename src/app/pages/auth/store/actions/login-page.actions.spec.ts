d7f640b23ab55dc6d9c7f9c6f356ee6d0036811a
Enea Jahollari <jahollarienea14@gmail.com>
2021-01-14 23:10:42 +0100

    ✔ Added Auth Actions Tests

---
+++ src/app/pages/auth/store/actions/login-page.actions.spec.ts
@@ -0,0 +1,23 @@
+import * as fromLoginPage from './login-page.actions';
+import { Credentials } from '@auth/models';
+
+describe('Login Page Actions', () => {
+
+  describe('Login Request', () => {
+    it('should create a Login action with credentials', () => {
+      let credentials: Credentials = { username: 'test', password: 'test' };
+      const action = fromLoginPage.login({ credentials });
+      expect(action.type).toEqual('[Login Page] Login Request');
+      expect(action.credentials).toEqual(credentials);
+      expect(action.credentials).not.toBeNull();
+    });
+  });
+
+  describe('Clear Login Error', () => {
+    it('should create a Clear Login Error action', () => {
+      const action = fromLoginPage.clearLoginError();
+      expect(action.type).toEqual('[Login Page] Clear Login Error');
+    });
+  });
+
+});
---
+++ src/app/pages/auth/store/actions/register-page.actions.spec.ts
@@ -0,0 +1,27 @@
+import * as fromRegisterPage from './register-page.actions';
+import { Register } from '@auth/models';
+
+describe('Register Page Actions', () => {
+
+  describe('Register', () => {
+    it('should create a Register action with credentials', () => {
+      let credentials: Register = {
+        username: 'test', birthday: new Date(),
+        city: 'T', country: 'T', gender: 'Male',
+        knownAs: 'T', password: 'TTTTT'
+      };
+      const action = fromRegisterPage.register({ credentials });
+      expect(action.type).toEqual('[Register Page] Register');
+      expect(action.credentials).toEqual(credentials);
+      expect(action.credentials).not.toBeNull();
+    });
+  });
+
+  describe('Clear Register Error', () => {
+    it('should create a Clear Register Page Error action', () => {
+      const action = fromRegisterPage.clearRegisterError();
+      expect(action.type).toEqual('[Register Page] Clear Register Page Error');
+    });
+  });
+
+});
