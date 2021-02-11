972f67b1f659249db9e1e3ecd4d1eed5d98c873f
Enea Jahollari <jahollarienea14@gmail.com>
2021-02-11 23:58:47 +0100

    😎 Update SettingsGuard to comply with the new settings selectors

--- src/app/pages/settings/guards/settings.guard.ts
+++ src/app/pages/settings/guards/settings.guard.ts
@@ -4,21 +4,19 @@ import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import { map } from 'rxjs/operators';
-import { Status, User } from '@core/models';
-import { SettingsActions } from '@settings/store/actions';
-import * as fromSettings from '@settings/store/reducers';
-import { SettingsState } from '@settings/store/reducers';
+import { selectUserSettingsState, SettingsState } from '@settings/store/reducers';
 import { loadEntity } from '@shared/helpers';
+import { loadUserSettings } from '@settings/store/actions/settings.actions';
 
-@Injectable({ providedIn: 'root' })
+@Injectable()
 export class SettingsGuard implements CanActivate {
 
   constructor(private store: Store<SettingsState>) {}
 
   canActivate(next: ActivatedRouteSnapshot): Observable<boolean> {
-    return this.store.select(fromSettings.selectUserDetails).pipe(
-      map((user: User & Status) => {
-        loadEntity(user, () => this.store.dispatch(SettingsActions.loadAuthDetails()))
+    return this.store.select(selectUserSettingsState).pipe(
+      map((userSettings) => {
+        loadEntity(userSettings, () => this.store.dispatch(loadUserSettings()))
         return true;
       })
     );
